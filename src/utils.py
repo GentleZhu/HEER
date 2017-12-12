2ae6630028a092e4ec571f105a94e020cce3c0eb
GentleZhu <544524199@qq.com>
2017-12-12 14:41:27 -0600

    symm layer

--- src/utils.py
+++ src/utils.py
@@ -23,3 +23,25 @@ class DiagLinear(nn.Module):
     def forward(self, input):
         # See the autograd section for explanation of what happens here.
         return input * self.weight
+
+class SymmLinear(nn.Module):
+    def __init__(self, input_features):
+        super(SymmLinear, self).__init__()
+        self.input_features = input_features
+
+        # nn.Parameter is a special kind of Variable, that will get
+        # automatically registered as Module's parameter once it's assigned
+        # as an attribute. Parameters and buffers need to be registered, or
+        # they won't appear in .parameters() (doesn't apply to buffers), and
+        # won't be converted when e.g. .cuda() is called. You can use
+        # .register_buffer() to register buffers.
+        # nn.Parameters can never be volatile and, different than Variables,
+        # they require gradients by default.
+        self.weight = nn.Parameter(torch.Tensor(input_features, input_features))
+
+        # Not a very smart way to initialize weights
+        self.weight.data.uniform_(-0.1, 0.1)
+
+    def forward(self, input):
+        # See the autograd section for explanation of what happens here.
+        return input * (self.weight.transpose() + self.weight)
